c3f44f2c1d3738553080bb059b7a82b386be75f2
takashiAg <crawd4274@gmail.com>
2020-01-21 19:32:00 +0900

    chamge command

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,12 @@
 import colors from 'vuetify/es5/util/colors'
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/nuxt_circleci_autodeploy/'
+        }
+      }
+    : {}
 
 export default {
   mode: 'spa',
@@ -9,20 +17,33 @@ export default {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: {
+    color: '#fff'
+  },
   /*
    ** Global CSS
    */
@@ -73,6 +94,7 @@ export default {
       }
     }
   },
+  routerBase,
   /*
    ** Build configuration
    */
